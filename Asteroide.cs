6e06474c223ebf1b93d7c82b96c3372dc66a9731
icelandia <ice-return@hotmail.com>
2023-11-16 12:08:23 -0300

    Clean slate

--- Asteroide.cs
+++ Asteroide.cs
@@ -11,8 +11,7 @@ namespace TerriblePrograma
         tamañoAsteroide tamaño;
         int[] pesoMineralesPorTipo;
         List<int> numeros;
-        string nombre = ":)";
-        string nombre = ":(";
+
 
         public Asteroide(tamañoAsteroide tamaño) : base()
         {
